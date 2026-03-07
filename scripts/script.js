33b6bb228b09eb24a6d99d68485e511bbc6482bd
Salemoche <gabrielmolembi@hotmail.com>
2026-03-07 14:07:23 +0100

    add swiper

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,29 @@ $(document).ready(function() {
     $('#target-1').toggleClass('active');
   })
 });
+
+/*=======================*
+ *  Swiper Slider
+ *=======================*/
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
